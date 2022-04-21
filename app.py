02786f3edec24eb940aec1741c0f3dd70989242f
Tashan Bedeau <me@tashan.co>
2022-04-21 14:30:04 +0100

    Added db models and database file

--- app.py
+++ app.py
@@ -5,6 +5,17 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///password_manager.db'
 db = SQLAlchemy(app)
 
+## Database Models
+
+class Password(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Password %r>' % self.id
+
+
 @app.route('/')
 def index():
     return render_template('index.html')

622aa096e28f8db068d63c5571ad3c9f0722f6eb
Tashan Bedeau <me@tashan.co>
2022-04-22 08:13:04 +0100

    Updated index page route to integrate form functionality

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///password_manager.db'
 db = SQLAlchemy(app)
 
 ## Database Models
-
 class Password(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
@@ -15,10 +14,24 @@ class Password(db.Model):
     def __repr__(self):
         return '<Password %r>' % self.id
 
-
+## Index Page Route
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        username = request.form['name']
+        password = request.form['password']
+
+        new_password = Password(username=username, password=password)
+        try:
+            db.session.add(new_password)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue with adding your password'
+    
+    else:
+        passwords = Password.query.order_by(Password.id).all()
+        return render_template('index.html', passwords=passwords)
 
 if __name__ == '__main__':
     app.run(debug=True)

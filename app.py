6a3d2ec2f2fe41e38d1f25b45bf7a8f9e788b5d1
Tashan Bedeau <me@tashan.co>
2022-04-23 09:59:05 +0100

    Added website db model

--- app.py
+++ app.py
@@ -10,9 +10,17 @@ class Password(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), unique=True, nullable=False)
-
+    website_id = db.Column(db.Integer, db.ForeignKey('website.id'), nullable=False)
+    
     def __repr__(self):
         return '<Password %r>' % self.id
+
+class Website(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+
+    def __repr__(self):
+        return '<Website %r>' % self.name
 
 ## Index Page Route
 @app.route('/', methods=['GET', 'POST'])

8d2e8747ea7d5209331801ab8edc5581ba312ecd
Tashan Bedeau <me@tashan.co>
2022-04-23 15:52:07 +0100

    Completed password generator

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,19 @@
-console.log("js has loaded!")
+$(document).ready(function () {
+
+    $('#generated-password--button').click(function(event) {
+        event.preventDefault()
+        var generatedPassword
+
+        url = '/get-a-new-password'
+        $.ajax({
+            url: url,
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                generatedPassword = data.password
+                console.log("Generated Password is ", generatedPassword)
+                $('#generated-password').val(generatedPassword)
+            }
+        })
+    })
+});
